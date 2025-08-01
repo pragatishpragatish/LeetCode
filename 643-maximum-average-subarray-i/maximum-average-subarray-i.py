804b2c26034504da7be60f653c6ed06d3c0d9b34
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-08-01 18:15:03 +0530

    Time: 87 ms (48.98%) | Memory: 27.5 MB (10.56%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        windowSum = sum(nums[:k])
+        maxSum = windowSum
+
+        for i in range(k, len(nums)):
+            windowSum += nums[i] - nums[i - k]
+            maxSum = max(maxSum, windowSum)
+
+        return maxSum / k
