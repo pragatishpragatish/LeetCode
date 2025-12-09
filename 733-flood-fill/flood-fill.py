b59c626d22badf1b9ed84c50aecebe0953e36edf
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-12-09 15:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (27.99%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,25 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols = len(image), len(image[0])
         iniCol = image[sr][sc]
         if iniCol == color:
             return image
-        seen = set()
 
-        def dfs(r,c):            
+        seen = set()
+        rows, cols = len(image), len(image[0])
+
+        def dfs(r,c):
+            if (r, c) in seen:
+                return
+            seen.add((r, c))
             if r < 0 or r >= rows or c < 0 or c >= cols:
                 return
-            if (r,c) in seen:
-                return
-            seen.add((r,c))
+            
             if image[r][c] != iniCol:
                 return
+            
             image[r][c] = color
 
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                if (r+dr, c+dc) not in seen:
-                    dfs(r+dr, c+dc)
+            dire = [[1,0], [-1,0], [0,1], [0,-1]]
+            for dc, dr in dire:
+                dfs(r+dr, c+dc)
 
         dfs(sr, sc)
         return image
