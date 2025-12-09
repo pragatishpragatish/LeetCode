9e0a98cb64621a8ea952bd3512491b2597712103
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-12-09 15:53:14 +0530

    Time: 27 ms (42.52%) | Memory: 19.3 MB (26.21%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         maxA = 0
-        seen = set()
         rows, cols = len(grid), len(grid[0])
 
         def dfs(r,c):
             if r < 0 or r >= rows or c < 0 or c >= cols:
                 return 0
 
-            if (r, c) in seen or grid[r][c] != 1:
+            if grid[r][c] != 1:
                 return 0
-            seen.add((r, c))
             
+            grid[r][c] = 0
             area = 1
             
             dire = [[0, 1], [0, -1], [1, 0], [-1, 0]]
@@ -22,7 +21,7 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                if (r, c) not in seen and grid[r][c] == 1:
+                if grid[r][c] == 1:
                     maxA = max(maxA, dfs(r, c))
 
         return maxA
