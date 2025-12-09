ff0a1587857843e778e268f1887a1dbd5c4b4dc0
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-12-09 15:51:50 +0530

    Time: 39 ms (14.09%) | Memory: 20.6 MB (5.67%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,28 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        maxArea = 0
-        visit = set()
+        maxA = 0
+        seen = set()
         rows, cols = len(grid), len(grid[0])
 
-        def dfs(r, c):
+        def dfs(r,c):
             if r < 0 or r >= rows or c < 0 or c >= cols:
                 return 0
-            if grid[r][c] != 1 or (r,c) in visit:
+
+            if (r, c) in seen or grid[r][c] != 1:
                 return 0
-
-            visit.add((r,c))
+            seen.add((r, c))
+            
             area = 1
-
-            directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
-            for dr, dc in directions:
-                area += dfs(r + dr, c + dc)
-
+            
+            dire = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in dire:
+                area += dfs(r+dr, c+dc)
+            
             return area
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and (r, c) not in visit:
-                    maxArea = max(dfs(r,c), maxArea)
+                if (r, c) not in seen and grid[r][c] == 1:
+                    maxA = max(maxA, dfs(r, c))
 
-        return maxArea
+        return maxA
