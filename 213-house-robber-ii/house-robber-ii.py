49f1f1dedaeb7a2b83060f87c345126c01a83d07
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-12-14 16:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -3,21 +3,16 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        if n == 2:
+        if n == 2 or n == 3:
             return max(nums)
 
-        dp1 = [0] * n
-        dp2 = [0] * n
+        dp1 = [0] * (n)
+        dp2 = [0] * (n)
+        dp1[0], dp1[1] = nums[0], max(nums[0], nums[1])
+        dp2[1], dp2[2] = nums[1], max(nums[1], nums[2])
 
-        # Case 1: houses [0 ... n-2]
-        dp1[0] = nums[0]
-        dp1[1] = max(nums[0], nums[1])
-        for i in range(2, n - 1):
+        for i in range(2, n-1):
             dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i])
-
-        # Case 2: houses [1 ... n-1]
-        dp2[1] = nums[1]
-        dp2[2] = max(nums[1], nums[2])
         for i in range(3, n):
             dp2[i] = max(dp2[i-1], dp2[i-2] + nums[i])
 
