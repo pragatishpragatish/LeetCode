50d43bc15d9ff9339c60cef0f8a4b76469bdf7ea
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2025-12-14 10:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (95.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-
-        def fib(n):
-            if n == 1:
-                return 1
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
             
-            if n == 2:
-                return 2
-            
-            if n in memo:
-                return memo[n]
-            if not n in memo:
-                memo[n] = fib(n-1) + fib(n-2)
+        dp = [0]*n
 
-            return fib(n-1) + fib(n-2)
-        return fib(n)
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[-1]
