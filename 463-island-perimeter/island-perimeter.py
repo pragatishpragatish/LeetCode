a629abd2184b3b849eb7e11b8387c83d03939b22
Pragatish A M <59360610+pragatishpragatish@users.noreply.github.com>
2026-01-27 17:04:31 +0530

    Time: 165 ms (11.02%) | Memory: 22.6 MB (18.55%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,10 +1,9 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        perimeter = 0
         rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
-            if r >= rows or r < 0 or c < 0 or c >= cols or grid[r][c] == 0:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
                 return 1
 
             if grid[r][c] == -1:
@@ -13,15 +12,13 @@ class Solution:
             grid[r][c] = -1
 
             peri = 0
-
-            dire = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            dire = [[1,0],[-1,0],[0,1],[0,-1]]
             for dr, dc in dire:
                 peri += dfs(r+dr, c+dc)
 
             return peri
 
-
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                   return dfs(r, c)
+                    return(dfs(r, c))
